785ff00a9616f8b309083f77c3c74b804d05dcbd
venky-17 <128626606+venky-17@users.noreply.github.com>
2023-03-28 15:22:57 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,24 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hoursHand = document.querySelector('.hour');
+    const minsHand = document.querySelector('.minute');
+    const secondsHand = document.querySelector('.second');
+
+    function setDate() {
+        const now = new Date();
+        const hour = now.getHours();
+        const minute = now.getMinutes();
+        const seconds = now.getSeconds();
+
+        const secondsDegree = ((seconds/60)*360);
+        const minuteDegree = ((minute/60)*360);
+        const hourDegree = ((hour/12)*360);
+
+        secondsHand.style.transform = `rotate(${secondsDegree}deg)`;
+        minsHand.style.transform = `rotate(${minuteDegree}deg)`;
+        hoursHand.style.transform = `rotate(${hourDegree}deg)`;
+    }
+
+    setInterval(setDate, 10);
+});
